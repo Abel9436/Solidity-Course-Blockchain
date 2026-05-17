dc994c5c218bbe9de3b303ef06448d9b4cc6de67
Abel Bekele <136198323+Abel9436@users.noreply.github.com>
2026-05-17 21:59:01 +0300

    Delete Module-1 Solidity-Introduction directory

--- Module-1 Solidity-Introduction/contract11.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract Contract {
-    function double(uint _x) external pure returns(uint result) {
-        result = _x * 2;
-    }
-}
--- Module-1 Solidity-Introduction/contract3.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract Contract {
-    int8 public a = 40;
-    int8 public b = -30;
-    int16 public difference = a - b;
-}
